c92db43401dc36166871326d722405237f5bba7c
Toky RT <tokyrt.pro@gmail.com>
2025-03-17 18:00:22 +0300

    initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+
+    local tsConfigs = require("nvim-treesitter.configs")
+    
+    tsConfigs.setup({
+      ensure_installed = {"c", "lua", "javascript", "vim", "vimdoc", "typescript", "css", "python", "scss"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
